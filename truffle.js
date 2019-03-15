93fdfa71aeab03832dcacf01e645362f9736a9f5
vsmelov <vladimir@block-tech.io>
2019-03-15 12:41:57 +0100

    * try to make it work

--- truffle.js
+++ truffle.js
@@ -12,5 +12,10 @@ module.exports = {
       gas: 0xfffffffffff,
       gasPrice: 0x01
     }
+  },
+  compilers: {
+     solc: {
+       version: "0.4.24"  // ex:  "0.4.20". (Default: Truffle's installed solc)
+     }
   }
 };
